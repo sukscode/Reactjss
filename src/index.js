c4d9d410e812264b89c96b4e88b7b93445145feb
Sukriti Srivastava <79142553+sukscode@users.noreply.github.com>
2022-02-18 23:16:36 +0530

    styling for react elements

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom";
 const fname = "Alpha";
 const lname = "Alpha";
 //const num = 7;
+const img = "https://picsum.photos/200";
+const custom = {
+  color: "red",
+  fontSize: "40px",
+  border: "3px solid black"
+};
 
 ReactDOM.render(
   <div>
-    <h1>Hello {fname + " " + lname}!</h1>
-    <h1>Hello {`${fname} ${lname}`}!</h1>
-    <p>My lucky number is {Math.floor(Math.random() * 10)}</p>
+    <h1 style={custom}>Hello {fname + " " + lname}!</h1>
+    <img src={img} />
   </div>,
   document.getElementById("root")
 );
