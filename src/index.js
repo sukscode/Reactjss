42899fc421f47a75eed48dd1ef92212a995ff4eb
Sukriti Srivastava <79142553+sukscode@users.noreply.github.com>
2022-02-19 14:06:04 +0530

    React Props

--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const date = new Date();
-const currentTime = date.getHours();
-let greeting;
-const custom = {
-  color: ""
-};
+import App from "./components/App";
 
-if (currentTime < 12) {
-  greeting = "Good Morning";
-  custom.color = "red";
-} else if (currentTime < 18) {
-  greeting = "Good Afternoon";
-  custom.color = "green";
-} else {
-  greeting = "Good Night";
-  custom.color = "blue";
-}
-
-ReactDOM.render(
-  <h1 className="heading" style={custom}>
-    {greeting}
-  </h1>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
