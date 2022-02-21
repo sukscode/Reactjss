b0188715b8f3ce6c346fcabb26724157fda514d9
Sukriti Srivastava <79142553+sukscode@users.noreply.github.com>
2022-02-21 16:54:15 +0530

    mapping

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+import Avatar from "./Avatar";
+import Detail from "./Detail";
 
 function Card(props) {
   return (
-    <div>
-      <div className="card">
-        <div className="top">
-          <h2 className="name">{props.name}</h2>
-          <img className="circle-img" src={props.img} />
-        </div>
-        <div className="bottom">
-          <p className="info">{props.phone}</p>
-          <p className="info">{props.email}</p>
-        </div>
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <Avatar img={props.img} />
+      </div>
+      <div className="bottom">
+        <Detail detailInfo={props.tel} />
+        <Detail detailInfo={props.email} />
       </div>
     </div>
   );
 }
+
 export default Card;
--- src/contacts.js
+++ src/contacts.js
@@ -1,7 +1,6 @@
-import App from "./components/App";
-import Card from "./components/Card";
 const contacts = [
   {
+    id: 1,
     name: "Beyonce",
     imgURL:
       "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
@@ -9,6 +8,7 @@ const contacts = [
     email: "b@beyonce.com"
   },
   {
+    id: 2,
     name: "Jack Bauer",
     imgURL:
       "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
@@ -16,6 +16,7 @@ const contacts = [
     email: "jack@nowhere.com"
   },
   {
+    id: 3,
     name: "Chuck Norris",
     imgURL:
       "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
@@ -23,4 +24,5 @@ const contacts = [
     email: "gmail@chucknorris.com"
   }
 ];
+
 export default contacts;
